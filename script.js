c8c37af0278854c33bc41b5b6e40bf183c7bec25
mpavlishyn-max <m.pavlishyn@tl.kyiv.ua>
2026-05-05 20:29:41 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// Завдання номер 1:
+// let a = 2 * -5 + 4;
+// document.writeln(a + "<br>");
+
+// let b = 2 * (-5 + 4);
+// document.writeln(b + "<br>");
+
+// let c = 7 % 3;
+// document.writeln(c + "<br>");
+
+// let d = 12 / 4 - 2;
+// document.writeln(d + "<br>");
+
+// let n = 12 / (4 - 2);
+// document.writeln(n + "<br>");
+
+// let m = 7 % 3 * -5;
+// document.writeln(m + "<br>");
+
+
+
+// Завдання номер 2:
+// let a = 2;
+// b = 3 * ++a;
+
+// document.writeln(b);
+
+
+
+// Завдання номер 3:
+// let c = 5;
+// d = (c++) + 4;
+
+// document.writeln(d);
+
+
+
+// Завдання номер 4:
